3707dda7eca57d97071ca5ae80914acaa855fc19
Nadia-Saeed <104302839+NadiaSaeed@users.noreply.github.com>
2024-07-21 18:32:19 +0500

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,16 +6,16 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-           "Bridging the Gap Between Humans and Machine",
-           "Unlocking the Secrets of Text"
+           "Explore the Possibilities with Dr. Nadia Saeed",
+           "Bridging the Gap Between Humans and Machines",
+           "Unlocking the Secrets of Text",
            "Where Language Meets Technology",
            "Transforming Text into Actionable Insights",
            "Let's Revolutionize Communication with NLP",
-           "Craft Intelligent Language Solutions with NLP.",
-           "Guide to the Fascinating World of NLP.",
-           "Explore the Possibilities with Dr. Nadia Saeed",
+           "Craft Intelligent Language Solutions with NLP",
+           "Guide to the Fascinating World of NLP",
            "Supercharge Communication and Information Access",
-           "Shaping Tomorrow with Innovative NLP Solutions."
+           "Shaping Tomorrow with Innovative NLP Solutions"
         ],
         autoStart: true,
         loop: true,
